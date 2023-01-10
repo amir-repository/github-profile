4bde97f78cbc4cbc896ac92ea5ce13d3cea8fe1f
amir-repository <achmadnassardhi@gmail.com>
2023-01-11 02:22:21 +0700

    feat: add heex template

--- lib/github_search_web/live/github_search_live.ex
+++ lib/github_search_web/live/github_search_live.ex
@@ -4,12 +4,4 @@ defmodule GithubSearchWeb.GithubSearchLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
-
-  def render(assigns) do
-    ~H"""
-    <h1 class="text-3xl">
-      Hello world!
-    </h1>
-    """
-  end
 end
