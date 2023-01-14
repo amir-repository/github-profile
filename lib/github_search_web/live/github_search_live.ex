e67d5f48182800e4a631af60b79cb909caee1bb2
amir-repository <achmadnassardhi@gmail.com>
2023-01-14 13:38:14 +0700

    add poison

--- lib/github_search_web/live/github_search_live.ex
+++ lib/github_search_web/live/github_search_live.ex
@@ -2,6 +2,10 @@ defmodule GithubSearchWeb.GithubSearchLive do
   use GithubSearchWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :username, "Derek")}
+  end
+
+  def handle_event("search_username", %{"username" => username}, socket) do
+    {:noreply, assign(socket, :username, username)}
   end
 end
