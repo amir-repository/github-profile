036fc7be8cbd52ec9c7df931de60a9f8e903b536
amir-repository <achmadnassardhi@gmail.com>
2023-01-14 14:23:05 +0700

    feat: add search

--- lib/github_search_web/live/github_search_live.ex
+++ lib/github_search_web/live/github_search_live.ex
@@ -2,10 +2,23 @@ defmodule GithubSearchWeb.GithubSearchLive do
   use GithubSearchWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :username, "Derek")}
+    {:ok, assign(socket, :user, [])}
   end
 
   def handle_event("search_username", %{"username" => username}, socket) do
-    {:noreply, assign(socket, :username, username)}
+    user = user_search(username)
+    IO.inspect(user)
+    {:noreply, assign(socket, :user, user)}
+  end
+
+  defp user_search(username) do
+    api_url = "https://api.github.com/users/"
+    {:ok, result} = HTTPoison.get(api_url <> username)
+    decode_json(result.body)
+  end
+
+  defp decode_json(json) do
+    {:ok, result} = Jason.decode(json)
+    result
   end
 end
