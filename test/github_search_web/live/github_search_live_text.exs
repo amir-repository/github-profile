1e4c8e9f19462bdb442041d04957dc0f76852caa
amir-repository <achmadnassardhi@gmail.com>
2023-01-16 01:36:55 +0700

    test: add toggle theme test

--- test/github_search_web/live/github_search_live_text.exs
+++ test/github_search_web/live/github_search_live_text.exs
@@ -3,9 +3,15 @@ defmodule GithubSearchWeb.GithubSearchLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "Initial theme", %{conn: conn} do
+  test "Dark/Light theme toogle", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
 
     assert render(view) =~ String.capitalize("DARK")
+    assert render(view) =~ "icon-moon.svg"
+    assert render(view) =~ "class=\"light\""
+
+    assert view |> element("#toggle_theme") |> render_click() =~ String.capitalize("LIGHT")
+    assert view |> element("#toggle_theme") |> render_click() =~ "icon-moon.svg"
+    assert view |> element("#toggle_theme") |> render_click() =~ "class=\"dark\""
   end
 end
